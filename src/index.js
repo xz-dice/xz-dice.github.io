1b617bbe846757e2dfb30989e9da211e442ec81b
xz-dice <xandicearmah@gmail.com>
2021-06-21 13:53:10 +0100

    created nav bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Route} from "react-router-dom";
 import './index.css';
-import App from './App';
+import NavigationBar from "./sections/NavigationBar"
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Route>
+      <NavigationBar/>
+      </Route>
   </React.StrictMode>,
   document.getElementById('root')
 );
