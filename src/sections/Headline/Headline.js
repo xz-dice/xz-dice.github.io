eeff2d22522d17deee2e793831c7fb6f73294717
xz-dice <xandicearmah@gmail.com>
2021-06-29 20:17:44 +0100

    Reverting to edb0a186a3462de105f703ee29aac563b8d32dda

---
+++ src/sections/Headline/Headline.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Headline = () => {
+    return (
+        <section id="about" className="about-me">
+            <div className="intro">
+                <div className="one">
+                    Xandice Armah is <span> a full-stack web developer</span>
+                </div>
+                <div className="two">
+                        and co-founder of <a href="http://galpals.club" target="_blank">Gal Pals.</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Headline
